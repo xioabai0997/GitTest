bd62cc2522c852bdd2ff3a8703c43052daa7bcd8
xiaobai0997 <xiaobai0997@gmail.com>
2024-01-11 10:10:12 +0800

    master commit4

--- src/main/java/GitTest/TestOne.java
+++ src/main/java/GitTest/TestOne.java
@@ -3,7 +3,7 @@ package GitTest;
 public class TestOne {
     public static void main(String[] args) {
         System.out.println("TestTwo");
-        System.out.println("Testabc");
+        System.out.println("TestABC");
         System.out.println("Test10086");
     }
 }
