1d98befa713273c2e64f7b6f62fd07a48413cb3c
xiaobai0997 <xiaobai0997@gmail.com>
2024-01-11 10:03:34 +0800

    yyyy commit

--- src/main/java/GitTest/TestOne.java
+++ src/main/java/GitTest/TestOne.java
@@ -4,5 +4,6 @@ public class TestOne {
     public static void main(String[] args) {
         System.out.println("TestTwo");
         System.out.println("Testabc");
+        System.out.println("Test10086");
     }
 }
